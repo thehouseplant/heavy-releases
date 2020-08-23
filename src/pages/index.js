a37089dce7168dc3014c7daddb66d785202e7507
thehouseplant <sean.collins@outlook.com>
2020-08-22 18:46:13 -0700

    Adding basic table properties and data placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,47 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Table from '../components/table'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
-)
+function IndexPage() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Band',
+        accessor: 'band',
+      },
+      {
+        Header: 'Album',
+        accessor: 'album',
+      },
+      {
+        Header: 'Type',
+        accessor: 'type',
+      },
+      {
+        Header: 'Genre',
+        accessor: 'genre',
+      },
+      {
+        Header: 'Release Date',
+        accessor: 'releaseDate',
+      },
+    ],
+    []
+  )
+
+  const data = []
+
+  return (
+    <Layout>
+      <SEO title="Heavy Releases" />
+      
+      <p>
+        <Table columns={columns} data={data} />
+      </p>
+    </Layout>
+  )
+}
 
 export default IndexPage
