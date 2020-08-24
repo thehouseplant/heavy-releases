5d1615cd4bb4d77612c18c4530cb60f22706567d
thehouseplant <sean.collins@outlook.com>
2020-08-23 18:39:44 -0700

    Add link to Github repository

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { GoMarkGithub } from 'react-icons/go'
 
 const Header = ({ siteTitle }) => (
-  <Navbar bg="dark" variant="dark" expand="lg">
+  <Navbar bg="dark" variant="dark" expand="lg" justify-content-between>
     <Link to="/">
       <Navbar.Brand as="span">{siteTitle}</Navbar.Brand>
     </Link>
@@ -14,6 +15,11 @@ const Header = ({ siteTitle }) => (
       <Nav className="mr-auto">
         <Link to="/">
           <Nav.Link as="span">Home</Nav.Link>
+        </Link>
+      </Nav>
+      <Nav>
+        <Link to="https://github.com/thehouseplant/heavy-releases">
+          <Nav.Link as="span"><GoMarkGithub size="2em" /></Nav.Link>
         </Link>
       </Nav>
     </Navbar.Collapse>
