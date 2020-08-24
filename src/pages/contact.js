526b87b82f0acfd1e5a13490dc608dc7b2082402
thehouseplant <sean.collins@outlook.com>
2020-08-23 19:21:24 -0700

    Add Google ReCAPTCHA protection

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Layout from '../components/layout'
 import { Button, Form } from 'react-bootstrap'
+import ReCAPTCHA from 'react-google-recaptcha'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const ContactFormPage = () => (
   <Layout>
+    <SEO title="Heavy Releases | Contact" />
     <h1>Contact</h1>
 
-    <Form name="Contact Form" method="POST" data-netlify="true" action="/thank-you">
+    <Form name="Contact Form" method="POST" data-netlify="true" data-netlify-recaptcha="true" action="/thank-you">
       <input type="hidden" name="form-name" value="Contact Form" />
       <Form.Group controlId="contactForm.name">
         <Form.Label>Name</Form.Label>
@@ -24,6 +28,7 @@ const ContactFormPage = () => (
         <Form.Label>Message</Form.Label>
         <Form.Control as="textarea" rows="3" name="message" placeholder="Enter your message" />
       </Form.Group>
+      <ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_KEY} />
       <Button variant="primary" type="submit">Send Message</Button>
     </Form>
   </Layout>
