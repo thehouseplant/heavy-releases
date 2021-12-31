729ab560beeab89728d89942ac8c583f59a149a3
thehouseplant <sean.collins@outlook.com>
2021-12-30 16:51:46 -0800

    Replace react-table with data.js grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,60 @@
 import React from 'react'
+import { html } from 'gridjs'
+import { Grid } from 'gridjs-react'
 
 import Layout from '../components/layout'
 import Seo from '../components/seo'
-import Table from '../components/table'
 import JSONData from '../../data/releases.json'
 
-function IndexPage() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Band',
-        accessor: 'band',
-      },
-      {
-        Header: 'Album',
-        accessor: 'album',
-      },
-      {
-        Header: 'Type',
-        accessor: 'type',
-      },
-      {
-        Header: 'Genre',
-        accessor: 'genre',
-      },
-      {
-        Header: 'Release Date',
-        accessor: 'date',
-      },
-    ],
-    []
-  )
-
+const IndexPage = () => {
   const data = JSONData
 
   return (
     <Layout>
       <Seo title="Heavy Releases" />
-      <h1>Releases</h1>
-      <p>
-        <Table columns={columns} data={data} />
-      </p>
+      <Grid 
+        columns={[
+          {
+            id: 'band',
+            name: 'Band',
+            width: '17%',
+            formatter: (_, row) => html(`<a href='${row.cells[1].data}' target='_new'>${row.cells[0].data}</a>`)
+          },
+          {
+            id: 'bandUrl',
+            hidden: true,
+          },
+          {
+            id: 'album',
+            name: 'Album',
+            width: '30%',
+            formatter: (_, row) => html(`<a href='${row.cells[3].data}' target='_new'>${row.cells[2].data}</a>`)
+          },
+          {
+            id: 'albumUrl',
+            hidden: true,
+          },
+          {
+            id: 'type',
+            name: 'Type',
+            width: '10%',
+          },
+          {
+            id: 'genre',
+            name: 'Genre',
+            width: '25%',
+          },
+          {
+            id: 'date',
+            name: 'Release',
+            width: '18%',
+          }
+        ]} 
+        data={data}
+        pagination={{
+          limit: 10,
+        }}
+      />
     </Layout>
   )
 }
