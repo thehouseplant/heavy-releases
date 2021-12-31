046f6d92bc35b6b76e1f6225bc6fea3c128f02f5
thehouseplant <sean.collins@outlook.com>
2021-12-30 16:49:43 -0800

    Remove old navigation links

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,13 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { GoMarkGithub } from 'react-icons/go'
 
 const Header = ({ siteTitle }) => (
-  <Navbar bg="dark" variant="dark" expand="lg" justify-content-between>
-    <Link to="/">
-      <Navbar.Brand as="span">{siteTitle}</Navbar.Brand>
-    </Link>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Link to="/">
-          <Nav.Link as="span">Home</Nav.Link>
-        </Link>
-        <Link to="/contact">
-          <Nav.Link as="span">Contact</Nav.Link>
-        </Link>
-      </Nav>
-      <Nav>
-        <Link to="https://github.com/thehouseplant/heavy-releases">
-          <Nav.Link as="span"><GoMarkGithub size="2em" /></Nav.Link>
-        </Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <nav className="container-fluid">
+    <ul>
+      <li>
+        <strong>{siteTitle}</strong></li>
+    </ul>
+  </nav>
 )
 
 Header.propTypes = {
